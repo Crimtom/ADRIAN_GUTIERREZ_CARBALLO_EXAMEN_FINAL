80457741b66da8cbd3e2e51d53ec40e21a2077e6
Crimcat <adriangutcarb@gmail.com>
2024-08-23 22:01:23 -0600

    full_mv

--- ADRIAN_GUTIERREZ_CARBALLO_EXAMEN_FINAL/Capa_logica/CLS_met_viajeros.cs
+++ ADRIAN_GUTIERREZ_CARBALLO_EXAMEN_FINAL/Capa_logica/CLS_met_viajeros.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 
 namespace ADRIAN_GUTIERREZ_CARBALLO_EXAMEN_FINAL.Capa_logica
@@ -73,6 +74,46 @@ namespace ADRIAN_GUTIERREZ_CARBALLO_EXAMEN_FINAL.Capa_logica
                 Conn.Close();
             }
 
+            return retorno;
+        }
+
+        public static int Editar(int codigo, string ced, string nombre, int age, string pais, CLS_registro reg_id, CLS_motivo_viaje mot)
+        {
+            int retorno = 0;
+            CLS_viajeros.cedula = ced;
+            CLS_viajeros.nom_viajero = nombre;
+            CLS_viajeros.edad = age;
+            CLS_viajeros.nacion = pais;
+            CLS_viajeros.viaje_id = reg_id;
+            CLS_viajeros.mot_id = mot;
+
+            string querry = "UPDATE TABLE viajeros SET cedula = @ced, nom_viajero = @nombre, edad = @age, nacion = @pais, viaje_id = @reg_id, mot_id = @mot WHERE id_viaje = @codigo";
+            SqlConnection Conn = new SqlConnection();
+            try
+            {
+                using (Conn = BDconexion.obtener_conexion())
+                {
+                    SqlCommand cmd = new SqlCommand(querry, Conn);
+
+                    cmd.Parameters.Add(new SqlParameter("@codigo", codigo));
+
+                    cmd.Parameters.Add(new SqlParameter("@ced", ced));
+                    cmd.Parameters.Add(new SqlParameter("@nombre", nombre));
+                    cmd.Parameters.Add(new SqlParameter("@age", age));
+                    cmd.Parameters.Add(new SqlParameter("@reg_id", reg_id));
+                    cmd.Parameters.Add(new SqlParameter("@mot", mot));
+
+                    retorno = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                retorno = -1;
+            }
+            finally
+            {
+                Conn.Close();
+            }
             return retorno;
         }
     }
